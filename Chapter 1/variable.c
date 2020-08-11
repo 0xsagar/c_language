32d2bc2dd25acda52c6f8411afa43c64f716490e
theroyalsagar <69432950+theroyalsagar@users.noreply.github.com>
2020-08-11 14:02:43 +0530

    Create variable.c

---
+++ Chapter 1/variable.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    float b = 9.5;
+    char c = 'j';
+    printf("The value of a is %d\n", a);
+    printf("The value of b is %f\n", b);
+    printf("The value of c is %c\n", c);
+    return 0;
+}
