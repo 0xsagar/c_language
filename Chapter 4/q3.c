dec8051ffbbd8b8c524a1325e893e40e77a89b80
Sagar Roy <royalsagar4377@gmail.com>
2020-09-26 21:23:49 +0530

    q3 comments

--- Chapter 4/q3.c
+++ Chapter 4/q3.c
@@ -2,13 +2,16 @@
 
 int main()
 {
-    int a,n;
+    int a, n;
     printf("Enter n numbers to be printed in reverse order\n");
-    scanf("%d", &n);
-    for ( a = n; a ; a--)
+
+    /*'n' means the number of first 'n' numbers to be printed in reverse order*/
+
+    scanf("%d", &n);    //read 'n' and add it to the program
+    for (a = n; a; a--) //start from 'n' to '1' reducing '1' each time
     {
-        printf("The number is %d\n", a);
+        printf("\t%d\n", a); //print 'n' natural numbers in reverse order
     }
-    
+
     return 0;
 }
