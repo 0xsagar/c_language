d3e2be2df1b6329a594398ee6c5bec9a9d66e0f4
Sagar Roy <royalsagar4377@gmail.com>
2020-09-26 21:08:34 +0530

    q1 comments

--- Chapter 4/q1.c
+++ Chapter 4/q1.c
@@ -3,11 +3,11 @@
 int main()
 {
     int i = 0;
-    while (i<=10)
+    while (i <= 10) // till 'i' is less than 10, the process will continue
     {
-        printf("%d\n", i+10);
-        i++;
+        printf("%d\n", i + 10); //add '10' in the final result
+        i++;                    //keep adding 1 to 'i' until the process is stopped
     }
-    
+
     return 0;
 }
