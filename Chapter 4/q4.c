6fbb3f0615ee0225bdb1b5291b34b9e08262fbe9
Sagar Roy <royalsagar4377@gmail.com>
2020-09-26 21:29:20 +0530

    q4 comments

--- Chapter 4/q4.c
+++ Chapter 4/q4.c
@@ -6,11 +6,11 @@
 int main()
 {
     int i, n;
-    printf("Enter an integer\n");
-    scanf("%d", &n);
-    for (i = 1; i <= 10; ++i)
+    printf("Enter an integer\n"); //enter the number of which you want a multiplication table
+    scanf("%d", &n);              //read the number and add it to program
+    for (i = 1; i <= 10; ++i)     //starting from 'i=1' till 'i=10' increasing +1 each time
     {
-        printf("%d * %d = %d\n", n, i, n * i);
+        printf("\t%d * %d = %d\n", n, i, n * i); //multiply the inserted number with 'i' will the program ends
     }
 
     return 0;
