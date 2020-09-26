f431a457d8b9e5b846abf1f7aadf5b41ba2369ab
Sagar Roy <royalsagar4377@gmail.com>
2020-09-26 21:33:42 +0530

    q5 comments

--- Chapter 4/q4.c
+++ Chapter 4/q4.c
@@ -10,7 +10,7 @@ int main()
     scanf("%d", &n);              //read the number and add it to program
     for (i = 1; i <= 10; ++i)     //starting from 'i=1' till 'i=10' increasing +1 each time
     {
-        printf("\t%d * %d = %d\n", n, i, n * i); //multiply the inserted number with 'i' will the program ends
+        printf("\t%d * %d = %d\n", n, i, n * i); //multiply the inserted number with 'i' till the program ends
     }
 
     return 0;
--- Chapter 4/q5.c
+++ Chapter 4/q5.c
@@ -6,12 +6,12 @@
 int main()
 {
     int i, n;
-    
-    printf("Enter an integer\n");
-    scanf("%d", &n);
-    for (i = 10; i >= 1; --i)
+
+    printf("Enter an integer\n"); //Reverse multiplication table will be printed of this number
+    scanf("%d", &n);              //read the number and add it to the program
+    for (i = 10; i >= 1; --i)     //starting from 'i=10' till 'i=1' reducing '1' each time
     {
-        printf("%d * %d = %d\n", n, i, n * i);
+        printf("\t%d * %d = %d\n", n, i, n * i); //the reverse mutliplication table of 'n' is printed
     }
     return 0;
 }
