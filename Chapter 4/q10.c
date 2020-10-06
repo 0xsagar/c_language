6c7ac5484323d438c4f5d5f80630ebe5ed0edf2b
Sagar Roy <royalsagar4377@gmail.com>
2020-10-07 01:01:58 +0530

    q10 comments

--- Chapter 4/q10.c
+++ Chapter 4/q10.c
@@ -1,17 +1,20 @@
+
+//Factorial of a number using 'while' loop
+
 #include <stdio.h>
 
 int main()
 {
     int factorial = 1, i = 1, n = 1;
-    printf("Enter the number you want factorial of: \n");
-    scanf("%d", &n);
-    while (i<=n)
+    printf("Enter the number you want factorial of: \n"); //enter number 'n'
+    scanf("%d", &n);                                      //read 'n' and add it to the program
+    while (i <= n)                                        //continue until 'i' is less than 'n'
     {
-        factorial*=i;
-        i++;
+        factorial *= i; //factorial = factorial * i
+        i++;            //increasing '1' at a time
     }
-    
-    printf("The factorial of %d is %d", n, factorial);
+
+    printf("The factorial of %d is %d", n, factorial); //print the factorial of 'n'
 
     return 0;
 }
