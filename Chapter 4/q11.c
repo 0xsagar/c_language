edb29b48c35cbd7a5ca8ec4ff7c6601354cd2b92
Sagar Roy <royalsagar4377@gmail.com>
2020-10-07 01:08:25 +0530

    q11 comments

--- Chapter 4/q11.c
+++ Chapter 4/q11.c
@@ -3,23 +3,23 @@
 int main()
 {
     int n, prime = 1;
-    printf("Enter the mumber to be checked\n");
-    scanf("%d", &n);
-    for (int i = 2; i < n; i++)
+    printf("\nEnter the mumber to be checked\n\n"); //enter a number to check if it's a prime or not
+    scanf("%d", &n);                                //read it and add it to the program
+    for (int i = 2; i < n; i++)                     //from 'i=2' till 'i<n' increasing '1' at a time
     {
-        if (n % i == 0)
+        if (n % i == 0) //check if the number is divisible by and other number b/w '2' and itself (it will give remainder '0')
         {
-            prime = 0;
-            break;
+            prime = 0; //if it's divisible then it's not a prime number
+            break;     //so break the loop
         }
     }
-    if (prime == 0)
+    if (prime == 0) //check and print
     {
-        printf("It is a not a prime number\n");
+        printf("\n%d is a not a prime number\n\n", n);
     }
     else
     {
-        printf("It is a prime number\n");
+        printf("\n%d is a prime number\n\n", n);
     }
 
     return 0;
