e9cee2a84cbd9a84840d4a89952875bef9cc8ea8
Sagar <royalsagar4377@gmail.com>
2020-11-16 11:42:13 +0530

    fabonacci series using recursion

--- data_structure/fibonacci.c
+++ data_structure/fibonacci.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
-int fibonacci(int a, int b)
+int fibonacci(int n)
 {
-    if ()
+    int a = 0, b = 1, nextTerm;
+    printf("\nPrinting Fabonacci Series\n");
+    for (int i = 0; i <= n; i++)
     {
-        /* code */
+        printf("%d\t", a);
+        nextTerm = a + b;
+        a = b;
+        b = nextTerm;
     }
 }
 
 int main()
 {
-
-    return 0;
+    int a;
+    printf("How many terms of Fabonacci Series you want to print?\n");
+    scanf("%d", &a);
+    fibonacci(a);
 }
